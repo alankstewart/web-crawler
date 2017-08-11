d7e152cf0344b13c21c16bc273b5cca1038c5223
Alan Stewart <alankstewart@gmail.com>
2017-08-11 12:18:12 +1000

    Altered test

--- src/test/java/alankstewart/webcrawler/service/WebPageTest.java
+++ src/test/java/alankstewart/webcrawler/service/WebPageTest.java
@@ -12,7 +12,7 @@ public class WebPageTest {
 
     @Test
     public void shouldCrawlUrl() throws Exception {
-        final WebPage webPage = new WebPage(new URI("http://repo1.maven.org/maven2/org/springframework/boot/spring-boot-parent"), 2);
+        final WebPage webPage = new WebPage(new URI("http://repo1.maven.org/maven2/org/springframework/boot/spring-boot-parent"), 1);
         assertThat(webPage, is(notNullValue()));
     }
 }
