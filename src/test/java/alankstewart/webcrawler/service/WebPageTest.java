85e973525295052904ef11ccd0893790f23d090a
Alan Stewart <alankstewart@gmail.com>
2017-08-11 12:19:30 +1000

    Enhanced test

--- src/test/java/alankstewart/webcrawler/service/WebPageTest.java
+++ src/test/java/alankstewart/webcrawler/service/WebPageTest.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 
 import java.net.URI;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class WebPageTest {
@@ -14,5 +13,6 @@ public class WebPageTest {
     public void shouldCrawlUrl() throws Exception {
         final WebPage webPage = new WebPage(new URI("http://repo1.maven.org/maven2/org/springframework/boot/spring-boot-parent"), 1);
         assertThat(webPage, is(notNullValue()));
+        assertThat(webPage.getChildren(), hasSize(greaterThan(0)));
     }
 }
